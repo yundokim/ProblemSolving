2214c1274a8e110ad89e1b2a1d7b71c2ca0d1780
yundokim <yundokim0405@naver.com>
2023-05-23 13:22:28 +0900

    Still Working on KOISTUDY

---
+++ KOISTUDY/1730/main.cpp
@@ -0,0 +1,32 @@
+// 05/23/2023
+
+#include <cstdio>
+
+int a[105][105];
+int n, m, cnt = 0;
+
+void print() {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void push(int i, int j) {
+    if (0 <= i && i < n && 0 <= j && j < m) a[i][j] = ++cnt;
+}
+
+int main() {
+    scanf("%d %d", &n, &m);
+
+    for (int i = 0; i < n + m; i++) {
+        for (int j = 0; j < n + m; j++) {
+            push(n - j - 1, m - i + j - 1);
+        }
+    }
+
+    print();
+    return 0;
+}
